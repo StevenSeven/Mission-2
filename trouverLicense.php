b8e516e2ee7a1b3c9d901fd4919a2f015ffa402b
HeinTouchableFR <aymericlhomme@orange.fr>
2018-12-19 13:35:53 +0100

    indentation
    modification de l'indentation des pages index et trouverLicence

--- trouverLicense.php
+++ trouverLicense.php
@@ -12,78 +12,92 @@
 
 </head>
 <html>
-    <body>
-		<?php
-		include "connexion.php";
-			echo '
-				<div class="row">
-					<div class="col-sm-8" style="background-color:lavender;">
-						<img class="img-logo" src="img/logo.png">
-					</div>
-					<div class="col-sm-4" style="background-color:lavenderblush;">Compte connecté + date et heure de connexion</div>
-				</div>
-				<div class="row" >
-					<div class="col-sm-2" style="background-color:lavender;">
-						<button type="button" id="btncreatelicences" class="btn btn-primary" style="margin-left:5px; margin-bottom:1px; width:210px;">Création des licences</button>
-						<button type="button" id="btnfoundlicences" class="btn btn-primary" style="margin-left:5px; margin-bottom:1px; width:210px;">Rechercher un(e) licencé(e)</button>
-						<button type="button" id="btnvoirclub" class="btn btn-primary" style="margin-left:5px; margin-bottom:1px; width:210px;">Rechercher un Club</button>
-					</div>
-					<div class="col-sm-10" style="background-color:lavenderblush;">';
+<body>
+<script language="javascript">
+    function envoie(adresse){
+        document.form.action = adresse;
+        document.form.submit();
+    }
+</script>
+<?php
+include "connexion.php";
+?>
+
+<div class="row">
+	<div class="col-sm-8" style="background-color:lavender;">
+		<img class="img-logo" src="img/logo.png">
+	</div>
+	<div class="col-sm-4" style="background-color:lavenderblush;">Compte connecté + date et heure de connexion</div>
+</div>
+<div class="row" >
+	<div class="col-sm-2" style="background-color:lavender;">
+		<button type="button" id="btncreatelicences" class="btn btn-primary" style="margin-left:5px; margin-bottom:1px; width:210px;">Création des licences</button>
+		<button type="button" id="btnfoundlicences" class="btn btn-primary" style="margin-left:5px; margin-bottom:1px; width:210px;">Rechercher un(e) licencé(e)</button>
+		<button type="button" id="btnvoirclub" class="btn btn-primary" style="margin-left:5px; margin-bottom:1px; width:210px;">Rechercher un Club</button>
+		<button type="button" id="btnaddclub" class="btn btn-primary" style="margin-left:5px; margin-bottom:1px; width:210px;">Ajouter un Club</button>
+	</div>
+	<div class="col-sm-10" style="background-color:lavenderblush;">
+		<div class="formulaire col-sm-12">
+			<span class="span-center">Liste des licenciés</span>
+				<form name="form" method="post">
+					<table align="left" style="width:50%;">
+						<tr>
+							<td width="20%"><label for="numlicence"> N° licence:</label></td>
+							<td width="50"><input name="debnumlicence"></td>
+							<td width="50"><input name="finnumlicence"></td>
+						</tr>
+						<tr>
+							<td width="20%"><label for="nom"> Nom:</label></td>
+							<td width="50"><input name="debnom"></td>
+							<td width="50"><input name="finnom"></td>
+						</tr>
+						<tr>
+							<td width="20%"><label for="club"> Club:</label></td>
+							<td width="50"><input name="debclub"></td>
+							<td width="50"><input name="finclub"></td>
+						</tr>
+					</table>
+				</form>
+				<button type="Submit" onclick="javascript:envoie('trouverLicense.php')">Valider</button>
+				<button>Annuler</button>
+				<button type="button" id="btncreatelicences">Créer</button>
+			</div>
+<?php				
+if(isset($_POST['debclub'])){
+?>	
+			<div class="col-sm-12">
+				<span> test</span>
+			</div>
+<?php				
+}								
+?>			
 						
-							echo'
-							<span class="span-center">Liste des licenciés</span>
-							
-							<form action="index.php?foundlicencie" method="post">
-								<table align="left" style="width:50%;">
-									<tr>
-										<td width="20%"><label for="numlicence"> N° licence:</label></td>
-										<td width="50"><input name="debnumlicence"></td>
-										<td width="50"><input name="finnumlicence"></td>
-									</tr>
-									<tr>
-										<td width="20%"><label for="nom"> Nom:</label></td>
-										<td width="50"><input name="debnom"></td>
-										<td width="50"><input name="finnom"></td>
-									</tr>
-									<tr>
-										<td width="20%"><label for="club"> Club:</label></td>
-										<td width="50"><input name="debclub"></td>
-										<td width="50"><input name="finclub"></td>
-									</tr>
-								</table>
-								<button type="Submit">Valider</button>
-							</form>
-							<button>Annuler</button>
-							<button type="button" id="btncreatelicences">Créer</button>
-								';
-							if(isset($_POST['debclub'])){
-								echo $_POST['finclub'];
-							}								
-						
-						
-echo'				</div>
-				</div>
-			';
-		
-		?>
+	</div>
+</div>
 	
-		 <script>
-			var btncreatelicences = document.getElementById('btncreatelicences');
-				btncreatelicences.addEventListener('click', function() {
-					document.location.href = 'creerLicense.php';
-				});
+<script>
+	var btncreatelicences = document.getElementById('btncreatelicences');
+	btncreatelicences.addEventListener('click', function() {
+		document.location.href = 'creerLicense.php';
+	});
 			
-			var btnfoundlicences = document.getElementById('btnfoundlicences');
-				btnfoundlicences.addEventListener('click', function() {
-					document.location.href = 'trouverLicense.php';
-				});
-			var btnvoirclub = document.getElementById('btnvoirclub');
-				btnvoirclub.addEventListener('click', function() {
-					document.location.href = 'trouverClub.php';
-				});
-		</script>
-		<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-		<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
-		<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
-	</body>
+	var btnfoundlicences = document.getElementById('btnfoundlicences');
+	btnfoundlicences.addEventListener('click', function() {
+		document.location.href = 'trouverLicense.php';
+	});
+			
+	var btnvoirclub = document.getElementById('btnvoirclub');
+	btnvoirclub.addEventListener('click', function() {
+		document.location.href = 'trouverClub.php';
+	});
+	
+    var btnaddclub = document.getElementById('btnaddclub');
+    btnaddclub.addEventListener('click', function() {
+        document.location.href = 'ajouterClub.php';
+    });
+</script>
+<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+</body>
 </html>
